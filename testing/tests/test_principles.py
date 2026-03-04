03058f56f8d1b70a709bf90470b5de01dd1c5492
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-03-04 16:39:25 +0800

    Added tests covering input parameters, + communication check

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -16,14 +16,27 @@ def test_addition_with_bug():
 def test_addition_duplicated():
     # is it real good test (relies on absense of + in add())
     assert add(2, 3) == 2 + 3
-def test_addition_overcomplicated():
-    # formally valid test but too slow
-    for i in range(0, 2 ** 32):
-        for j in range(0, 2 ** 32):
-            assert add(i, j) == sum([i, j]) #might violate duplicate principles
-            assert add(-i, j) == sum ([-i, j])
-            assert add(i, -j) == sum ([i, -j])
-            assert add(-i, -j) == sum ([-i, -j])
+# def test_addition_overcomplicated():
+#     # formally valid test but too slow
+#     for i in range(0, 2 ** 32):
+#         for j in range(0, 2 ** 32):
+#             assert add(i, j) == sum([i, j]) #might violate duplicate principles
+#             assert add(-i, j) == sum ([-i, j])
+#             assert add(i, -j) == sum ([i, -j])
+#             assert add(-i, -j) == sum ([-i, -j])
+def test_addition_reasonable():
+    assert add(2, 2) == 4
+    assert add(0, 0) == 0
+    assert add(6, 7) == 13
+    assert add(-6, -7) == -13
+    assert add(6, -7) == -1
+    assert add(-6, 7) == 1
+    assert add(-7, 0) == -7
+    assert add(7, 0) == 7
+    print("Test ADDITION REASONABLE PASS")
+def test_addition_commutative():
+    assert add(7, -6) == 1
+    assert add(-6, 7) == 1
 
 
 
@@ -31,4 +44,6 @@ if __name__== "__main__":
     test_addition()
     test_addition_with_bug()
     test_addition_duplicated()
-    test_addition_overcomplicated() # too redundant run on your fear and risk
+    # test_addition_overcomplicated() # too redundant run on your fear and risk
+    test_addition_reasonable()
+    test_addition_commutative()
