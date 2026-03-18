767250bc7631f48d10a7b24847a80c4f818e8d9f
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-03-18 14:32:34 +0800

    Added tests for amounts above 20 and 50 million

--- testing_tdd/tests/test_ndfl_basic.py
+++ testing_tdd/tests/test_ndfl_basic.py
@@ -8,6 +8,13 @@ def test_ndfl_tier_2():
 def test_ndfl_tier_3():
     assert calculate_ndfl_tax(10_000_000) == 1_602_000
 
+def test_ndfl_tier_4():
+    assert calculate_ndfl_tax(25_000_000) == 4_402_000
+
+def test_ndfl_tier_5():
+    assert calculate_ndfl_tax(75_000_000) == 14_902_000
+
+
 
 
 
