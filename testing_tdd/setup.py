4b2fb5527537829f5de9e750520ce6f21329309a
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-04-15 15:41:24 +0800

    style: added explanatory comments

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -1,12 +1,10 @@
-from setuptools import (
-    setup,
-    find_packages
-)
+from setuptools import setup, find_packages
 
-setup (
-    name = "ndfl",
-    version  = "0.0.0",
-    package_dir = {"": "src"},
-    packages = find_packages(where="src")
-
+setup(
+    name="ndfl-starashchuk-v1",
+    version="0.0.0",
+    long_description="Tax ndfl calculator",
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
 )
